3485220a91d4baed9029a22331e909a8bff2d1dd
Hunter Krieger <hunterk31@hotmail.com>
2017-08-10 11:47:10 -0400

    change database URI

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eanmrvhallmyjz:90029caff260aad01581dfaf11bd2d5e654a8940322543564110b61021895789@ec2-23-21-85-76.compute-1.amazonaws.com:5432/d6328ucq1kvuvj'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nqrumzgzdzhlcn:195ad27bae6fcc9cf1f9754c9fea7502926a73cb0dcc54edd9fc477bc96539bb@ec2-54-163-227-202.compute-1.amazonaws.com:5432/dbjvasau17m931'
 
 app.config['SECRET_KEY'] = 'thiskeyissupersecret!'
 
